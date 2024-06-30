2a5a7bf28e23ccb9551589e5aba9206be6251f20
Sthiti Pragyan Panda <90833329+sthiti24@users.noreply.github.com>
2024-06-30 07:38:16 +0530

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -6,7 +6,7 @@ def sidebar():
     job_title = st.sidebar.selectbox("Select role:", [
         "Software Developer", "Software Engineer", "Cyber security", "Data analyst", "Data scientist","System Analyst",
         "Network Administrator","Database Administrator","Full Stack Developer","Frontend Developer","Backend Developer",
-        "Cloud Engineer","Mobile Developer","Android Developer","Web Developer","Application Engineer","Product Manager"]
+        "Cloud Engineer","Mobile Developer","Android Developer","Web Developer","Application Engineer","Product Manager"
     ])
     country = st.sidebar.selectbox("Select country:", ["United Kingdom", "United States", "Austria", "Australia", "Belgium", "Brazil", "Canada",
                                                        "Switzerland", "Germany", "Spain", "France", "India", "Italy", "Mexico", "Netherlands", "New Zealand", "Poland", "South Africa", "Singapore"])
